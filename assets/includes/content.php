f6fb73f639a21e2104ea540709c0a81d1aa265fc
Kenneth Lim Solomon <synecders@gmail.com>
2020-02-20 16:23:26 +0800

    add read more to the card

--- assets/includes/content.php
+++ assets/includes/content.php
@@ -13,6 +13,13 @@ if (mysqli_num_rows($result) > 0) {
         $content = $row["content"];
         $date = $row["date"];
 
+        $string = $content;
+        if (strlen($string) > 150) {
+            $stringCut = substr($string, 0, 150);
+            $endPoint = strrpos($stringCut, ' ');
+            $string = $endPoint ? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+            $string .= '... <a style="cursor: pointer; font-weight:bold;" href="" ><br>Read More</a>';
+        }
         echo '
             <div class="col-md-4">
                 <div class="card card-member">
@@ -23,11 +30,11 @@ if (mysqli_num_rows($result) > 0) {
                         <div class="description">
                         <a href=""><h3 class="title">' . $title . '</h3></a>
                             <p class="small-text">by ' . $author . '</p>
-                            <p class="description">' . $content . '</p>
+                            <p class="description">' . $string . '</p>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>  
             ';
     }
 } else {
