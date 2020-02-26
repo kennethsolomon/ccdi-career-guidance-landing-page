b77889c35e9604cac3eaa9378ffa4cb4c65b3971
Kenneth Solomon <synecders@gmail.com>
2020-02-27 01:03:11 +0800

    add courses offered

--- index.php
+++ index.php
@@ -98,46 +98,42 @@ include_once('./assets/includes/connection.php');
         </div>
     </div>
 
+    <div class="title-area">
+        <center>
+            <h1>Services</h1>
+        </center>
+        <div class="separator separator-danger">✻</div>
+    </div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="title-area">
-                    <h2>Our Services</h2>
-                    <div class="separator separator-danger">✻</div>
-                    <p class="description">We promise you a new look and more importantly, a new attitude. We build that by getting to know you, your needs and creating the best looking clothes.</p>
-                </div>
+        <div class="carousel-inner">
+            <div class="item active">
+                <img style="background-size: cover" src="assets/img/Slides/1.jpg" alt="Los Angeles">
             </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-graph2"></i>
-                        </div>
-                        <h3>Sales</h3>
-                        <p class="description">We make our design perfect for you. Our adjustment turn our clothes into your clothes.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-note2"></i>
-                        </div>
-                        <h3>Content</h3>
-                        <p class="description">We create a persona regarding the multiple wardrobe accessories that we provide..</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="info-icon">
-                        <div class="icon text-danger">
-                            <i class="pe-7s-music"></i>
-                        </div>
-                        <h3>Music</h3>
-                        <p class="description">We like to present the world with our work, so we make sure we spread the word regarding our clothes.</p>
-                    </div>
-                </div>
+
+            <div class="item">
+                <img src="assets/img/Slides/2.jpg" alt="Chicago">
             </div>
+
+            <div class="item">
+                <img src="assets/img/Slides/3.jpg" alt="New York">
+            </div>
+
+            <div class="item">
+                <img src="assets/img/Slides/4.jpg" alt="New York">
+            </div>
+
         </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="fa fa-chevron-left glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="fa fa-chevron-right glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 
 
@@ -178,7 +174,7 @@ include_once('./assets/includes/connection.php');
             <div class="title-area">
                 <!-- <h5 class="subtitle text-gray">Here are some</h5> -->
                 <h2>CCDI Administrators</h2>
-                <div class="separator separator-danger">∎</div>
+                <div class="separator separator-danger">✻</div>
             </div>
 
             <ul class="nav nav-text" role="tablist">
